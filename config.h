b8ae3292ab50fba92e27496d0b07fb51a382f681
PhilipRoman <frfilips@gmail.com>
2023-12-30 18:29:44 +0200

    Increase font size for high DPI

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Droid Sans Mono:style=Regular:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Droid Sans Mono:style=Regular:pixelsize=19:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
